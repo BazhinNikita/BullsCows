61c5dc01e9606fbab8552402de10f22887f95102
Vendetta <atuchin.s55@gmail.com>
2017-05-30 20:36:52 +0700

    Write rules

--- Main.c
+++ Main.c
@@ -25,6 +25,17 @@ void Menu(int *item) {
 	clear();
 }
 
+void GameRules()  {
+	clear();
+	Logo();
+	printw("\t\t  +-+-+-+-+-+\n");
+ 	printw("\t\t  |R|U|L|E|S|\n");
+ 	printw("\t\t  +-+-+-+-+-+\n");
+ 	printw("The numerical version of the game is usually played with 4 digits, but can also be played with 3 or any other number of digits\nBy keybord the players each input a 4(5- or 6-)-digit secret number. The digits must be all different. Then, in turn, the players try to guess their opponent's number who gives the number of matches. If the matching digits are in their right positions, they are bulls, if in different positions, they are cows .\nExample:\nSecret number: 4271\nOpponent's try: 1234\nAnswer: 1 bull and 2 cows. (The bull is 2, the cows are 4 and 1.)\nThe first one to reveal the other's secret number wins the game.\n");
+ 	getch();
+	clear();
+}
+
 
 int main() {
 	initscr(); 
