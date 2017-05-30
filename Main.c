6988487de470718db3e705f6add78ede47eb0103
fil-ondre <filipilila87@gmail.com>
2017-05-30 19:38:12 +0700

    Create Main

---
+++ Main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ncurses.h>
+
+int main() {
+	initscr(); 
+	scrollok(stdscr, TRUE); 
+	noecho(); 
+	int item;
+	int lvl = 0;
+	while (1) {
+		Menu(&item);
+		if (item == 1) Game(&lvl);
+		if (item == 2) GameRules();
+		if (item == 3) DifficultyLVL(&lvl);		
+		if (item == 4) {
+			endwin();
+			return 0;
+		}
+		if (item == 5) Authors();
+	}
+	return 0;
+}
+
